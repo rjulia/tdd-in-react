05087350d53a1f13d8a0932499413e6a8af367d4
ramon <reimon1972@gmail.com>
2022-11-26 22:17:27 +0800

    testing my list component

---
+++ src/NewMessageForm.spec.js
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import NewMessageForm from './NewMessageForm';
+
+describe('<NewMessageForm />', () => {
+  describe('clicking the send button', () => {
+    let sendHandler;
+    async function sendMessage() {
+      sendHandler = jest.fn().mockName('sendHandler');
+      render(<NewMessageForm onSend={sendHandler} />)
+
+      await userEvent.type(
+        screen.getByTestId('messageText'),
+        'New message',
+      );
+      userEvent.click(screen.getByTestId('sendButton'));
+    }
+
+    it('calls the send handler', async () => {
+      await sendMessage();
+      expect(sendHandler).toHaveBeenCalledWith('New message');
+    });
+  });
+});
